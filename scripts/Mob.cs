9669234ef42f78566d3abe9105cb8032f3735db2
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-10-01 22:18:02 +0200

    Add initial mob functionality

---
+++ scripts/Mob.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+public partial class Mob : CharacterBody3D
+{
+    [Export]
+    public int MinSpeed { get; set; } = 10;
+    [Export]
+    public int MaxSpeed { get; set; } = 18;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        MoveAndSlide();
+    }
+
+    public void Initialize(Vector3 startPosition, Vector3 playerPosition)
+    {
+        LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
+        RotateY((float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4));
+        
+        var randomSpeed = GD.RandRange(MinSpeed, MaxSpeed);
+        
+        Velocity = Vector3.Forward * randomSpeed;
+        Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
+    }
+
+    private void OnVisibilityNotifierScreenExited()
+    {
+        QueueFree();
+    }
+}
